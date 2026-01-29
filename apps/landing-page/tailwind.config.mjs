b934c86cea919f0057b45dbc9598ed999e0bc6a5
Pranavkumar <dpranav7745@gmail.com>
2026-01-29 19:59:06 +0530

    herosection is now implmented using lenis for scroll and the skew fpr text animation

--- apps/landing-page/tailwind.config.mjs
+++ apps/landing-page/tailwind.config.mjs
@@ -7,5 +7,24 @@ export default {
         './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
         '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
     ],
+    theme: {
+        extend: {
+            ...sharedConfig.theme?.extend,
+            animation: {
+                ...sharedConfig.theme?.extend?.animation,
+                'gradient': 'gradient 8s linear infinite',
+            },
+            keyframes: {
+                ...sharedConfig.theme?.extend?.keyframes,
+                'gradient': {
+                    '0%, 100%': {
+                        'background-position': '0% 50%',
+                    },
+                    '50%': {
+                        'background-position': '100% 50%',
+                    },
+                },
+            },
+        },
+    },
 }
-

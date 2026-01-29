3b1a099f95a08017e64a49d07e2a3d03621fc68a
Pranavkumar <dpranav7745@gmail.com>
2026-01-29 19:36:42 +0530

    feat : added the lenis for the  smooth scroll - using the themes varibles for the @repo/ui -  tailwind-animate and the lucide-astro for the icons

--- apps/landing-page/tailwind.config.mjs
+++ apps/landing-page/tailwind.config.mjs
@@ -1,8 +1,11 @@
+import sharedConfig from "../../packages/ui/tailwind.config.js";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+    ...sharedConfig,
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}',
+        '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
+    ],
 }
+
